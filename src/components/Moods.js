070c5f1d622b43e4f78470b3d0ba699df72fb086
Michael Maton <mjmaton6@gmail.com>
2020-11-02 13:59:48 -0600

    Spinner.js complete

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -51,13 +51,13 @@ export default function Moods() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: mood === happyMood ? "royalblue" : "crimson", /* STEP 2 */
+    color: mood === happyMood ? "royalblue" : "crimson"
   };
 
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>{mood}</div> {/* STEP 3 */}
+      <div id='mood' style={style}>{mood}</div>
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
